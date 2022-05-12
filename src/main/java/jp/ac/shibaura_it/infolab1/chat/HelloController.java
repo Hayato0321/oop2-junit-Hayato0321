9ee994bb5b7801cc9812924c37246a33c132c86f
bp20033 <bp20033@shibaura-it.ac.jp>
2022-05-12 17:55:40 +0900

    added Hello class.

--- src/main/java/jp/ac/shibaura_it/infolab1/chat/HelloController.java
+++ src/main/java/jp/ac/shibaura_it/infolab1/chat/HelloController.java
@@ -1,10 +1,15 @@
+package jp.ac.shibaura_it.infolab1.chat;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.time.LocalDateTime;
+
 @Controller
 public class HelloController {
-    @RequestMapping("/helloController")
+    @RequestMapping("/helloworld")
     public String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";
