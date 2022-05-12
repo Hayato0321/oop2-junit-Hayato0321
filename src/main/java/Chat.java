8943e4ae7e2a334371b0b205937f4e926eead6f6
bp20033 <bp20033@shibaura-it.ac.jp>
2022-05-12 16:41:06 +0900

    added Hello class.

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,4 +1,3 @@
-package jp.ac.shibaura_it.infolab1.chat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
