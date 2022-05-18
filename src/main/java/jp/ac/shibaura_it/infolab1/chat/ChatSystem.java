3a1abe37552788ff555b31e88405b004e042ec88
bp20033 <bp20033@shibaura-it.ac.jp>
2022-05-18 17:45:38 +0900

    added Hello class.

--- src/main/java/jp/ac/shibaura_it/infolab1/chat/ChatSystem.java
+++ src/main/java/jp/ac/shibaura_it/infolab1/chat/ChatSystem.java
@@ -1,28 +1,23 @@
 package jp.ac.shibaura_it.infolab1.chat;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.List;
-
 @Controller
-public class Chat {
-    private List<String> chats = new ArrayList(List.of("Kenji : Hello", "Dai : Hello,too!"));
-
-    @RequestMapping(value="/hello")
-    String hello(Model model) {
+public class ChatSystem {
+    private List<String> chats = new ArrayList();
+    @RequestMapping(value="/chat")
+    String hello(Model model){
         model.addAttribute("chats", chats);
-        return "home.html";
+        return "Csystem.html";
     }
-
     @RequestMapping(value="/add")
     String addChat(@RequestParam("name") String name,
                    @RequestParam("chat") String chat) {
-        chats.add(name + " : " + chat);
-        return "redirect:/hello";
+        chats.add(name + ":"+ chat);
+        return "redirect:/chat";
     }
+
 }
